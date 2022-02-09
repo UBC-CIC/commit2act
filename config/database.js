fd446596ba103d639af3f0ec4229723b24a0a2cc
Christy Lam <christy.lam@ubc.ca>
2022-02-09 12:44:24 -0800

    Update knex, objection, pg dependencies

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", ""),
+      database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
     },
