9970c7762aeee66196eb655911c82bb6caca930f
Christy Lam <christy.lam28@gmail.com>
2022-07-27 15:52:16 -0700

    Add parameter store custom resource

---
+++ amplify/backend/custom/parameterstoreGraphlQL/cdk-stack.ts
@@ -0,0 +1,52 @@
+import * as cdk from '@aws-cdk/core';
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import * as ssm from '@aws-cdk/aws-ssm';
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+
+
+export class cdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    new cdk.CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+    /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+    // Access other Amplify Resources 
+    
+    const dependencies:AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this, 
+      amplifyResourceProps.category, 
+      amplifyResourceProps.resourceName, 
+      [
+        {category: 'api', resourceName: "commit2act"},
+        {category: 'storage', resourceName: "s3commit2actstorage4f79922d"},
+        {category: 'auth', resourceName: "commit2act" },
+      ]
+    );
+    const GraphQLAPIIdOutput = cdk.Fn.ref(dependencies.api.commit2act.GraphQLAPIIdOutput)
+    const GraphQLAPIEndpointOutput = cdk.Fn.ref(dependencies.api.commit2act.GraphQLAPIEndpointOutput)
+    const BucketNameOutput = cdk.Fn.ref(dependencies.storage.s3commit2actstorage4f79922d.BucketName);
+    const UserPoolIdOutput = cdk.Fn.ref(dependencies.auth.commit2act.UserPoolId);
+    /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+    new ssm.StringParameter(this, 'ParameterStoreGraphQLAPIIdOutput', {
+      parameterName: 'GraphQLAPIIdOutput',
+      stringValue: GraphQLAPIIdOutput,
+    });
+    new ssm.StringParameter(this, 'ParameterStoreGraphQLAPIEndpointOutput', {
+      parameterName: 'GraphQLAPIEndpointOutput',
+      stringValue: GraphQLAPIEndpointOutput,
+    });
+
+    new ssm.StringParameter(this, 'ParameterStoreBucketNameOutput', {
+      parameterName: 'BucketNameOutput',
+      stringValue: BucketNameOutput,
+    });
+    new ssm.StringParameter(this, 'ParameterStoreUserPoolIdOutput', {
+      parameterName: 'UserPoolIdOutput',
+      stringValue: UserPoolIdOutput,
+    });
+    
+    
+  }  
+  }
