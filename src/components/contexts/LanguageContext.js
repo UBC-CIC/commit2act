31d9136772a8a517c4b763cdbc21b6fba9cf7d15
Cristian Rojas <chivocarc@hotmail.com>
2022-11-24 17:49:41 -0500

    Recognize language based on the browser

--- src/components/contexts/LanguageContext.js
+++ src/components/contexts/LanguageContext.js
@@ -3,8 +3,49 @@ import React, { useState, useContext } from 'react'
 const LanguageContext = React.createContext();
 export const useLanguageContext = () => useContext(LanguageContext);
 
+function getBrowserLocales(options = {}) {
+
+	const defaultOptions = {
+		languageCodeOnly: false,
+	};
+
+	const opt = {
+		...defaultOptions,
+		...options,
+	};
+
+	const browserLocales =
+		navigator.languages === undefined
+		? [navigator.language]
+		: navigator.languages;
+
+	if (!browserLocales) {
+		return undefined;
+	}
+
+	return browserLocales.map(locale => {
+		const trimmedLocale = locale.trim();
+		return opt.languageCodeOnly
+			? trimmedLocale.split(/-|_/)[0]
+			: trimmedLocale;
+	});
+}
+
+function getDefaultLocale(browserLocale) {
+	const fallbackLanguage = 'en', supportedLanguages = ['en', 'fr', 'es'];
+
+	if (supportedLanguages.indexOf(browserLocale) > -1) {
+		return browserLocale;
+	}
+
+	return fallbackLanguage;
+}
+
 export default function LanguageContextProvider({ children }) {
-	const [language, changeLanguage] = useState('en');
+	const browserLanguages = getBrowserLocales({languageCodeOnly: true});
+	const locale = getDefaultLocale(browserLanguages[0]);
+
+	const [language, changeLanguage] = useState(locale);
 	return (
 		<LanguageContext.Provider value={{ language, changeLanguage }}>
 			{children}
