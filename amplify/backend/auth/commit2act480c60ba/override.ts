7e406e5dc3c87bd31a481aa99152e1547b53a77d
Usama Habib Ch <43724683+usamahabib95@users.noreply.github.com>
2023-03-27 14:47:07 +0500

    chore: configuring facebook signin (#26)

--- amplify/backend/auth/commit2act480c60ba/override.ts
+++ amplify/backend/auth/commit2act480c60ba/override.ts
@@ -1,24 +1,55 @@
-import { AmplifyAuthCognitoStackTemplate, AmplifyProjectInfo } from '@aws-amplify/cli-extensibility-helper';
+import {
+  AmplifyAuthCognitoStackTemplate,
+  AmplifyProjectInfo,
+} from '@aws-amplify/cli-extensibility-helper';
 
-export function override(resources: AmplifyAuthCognitoStackTemplate, amplifyProjectInfo: AmplifyProjectInfo) {
-    resources.addCfnResource({
-        type:"AWS::Cognito::UserPoolIdentityProvider",
-        properties: {
-            AttributeMapping: {
-                preferred_username: "email",
-                name: "name",
-            },
-            ProviderDetails:{
-                client_id: "497146325730-sjbdj86ov2mch3l0rdtuce333cal9n3n.apps.googleusercontent.com",
-                client_secret: "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",
-                authorize_scopes: "openid email profile",
-            },
-            ProviderName: "Google",
-            ProviderType: "Google",
-            UserPoolId: {
-                Ref: "UserPool"
-            }
-        }},
-        "google-social-provider"
-    )
+export function override(
+  resources: AmplifyAuthCognitoStackTemplate,
+  amplifyProjectInfo: AmplifyProjectInfo
+) {
+  resources.addCfnResource(
+    {
+      type: 'AWS::Cognito::UserPoolIdentityProvider',
+      properties: {
+        AttributeMapping: {
+          preferred_username: 'email',
+          name: 'name',
+        },
+        ProviderDetails: {
+          client_id:
+            '497146325730-sjbdj86ov2mch3l0rdtuce333cal9n3n.apps.googleusercontent.com',
+          client_secret: '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',
+          authorize_scopes: 'openid email profile',
+        },
+        ProviderName: 'Google',
+        ProviderType: 'Google',
+        UserPoolId: {
+          Ref: 'UserPool',
+        },
+      },
+    },
+    'google-social-provider'
+  );
+  resources.addCfnResource(
+    {
+      type: 'AWS::Cognito::UserPoolIdentityProvider',
+      properties: {
+        AttributeMapping: {
+          preferred_username: 'email',
+          name: 'name',
+        },
+        ProviderDetails: {
+          client_id: '1250783402535608',
+          client_secret: 'a169008e6c7a335591360bf3b3549998',
+          authorize_scopes: 'openid email profile',
+        },
+        ProviderName: 'Facebook',
+        ProviderType: 'Facebook',
+        UserPoolId: {
+          Ref: 'UserPool',
+        },
+      },
+    },
+    'facebook-social-provider'
+  );
 }
