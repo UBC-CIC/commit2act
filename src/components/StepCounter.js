b4652a25d62769f12ce184c8dcf69d6293f00ebe
Stephen Belyea <belyea.stephen@gmail.com>
2023-12-06 14:56:19 -0500

    Larger screen styling, replace Stepper with StepCounter component

--- src/components/StepCounter.js
+++ src/components/StepCounter.js
@@ -3,54 +3,51 @@ import { visuallyHidden } from '@mui/utils';
 import { LOG_STEPS, TOTAL_LOG_STEPS } from '../constants/log-steps';
 import useTranslation from './customHooks/translations';
 
-const getStepStatus = (activeStep, currentStep) => {
-  const stepStatus = {
-    className: '',
-    statusText: null,
-  };
-  if (currentStep === activeStep) {
-    stepStatus.className = 'current';
-    stepStatus.statusText = 'logActionStepCurrent';
-  }
-  if (currentStep < activeStep) {
-    stepStatus.className = 'completed';
-    stepStatus.statusText = 'logActionStepCompleted';
-  }
-  return stepStatus;
-};
-
 const getStepCounterStyles = (currentColor) => ({
-  width: 'max-content',
-  margin: '1em',
+  margin: '1em auto',
   textAlign: 'center',
   fontSize: {
     xs: '0.725rem',
+    md: '0.825rem',
+    lg: '1rem',
   },
   ol: {
     display: 'flex',
-    justifyItems: 'space-between',
-    gap: '0.75em',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
     listStyle: 'none',
     padding: '0',
     margin: '0',
+    gap: {
+      xs: '0.75em',
+      lg: '1.25em',
+    },
   },
   li: {
     color: 'white',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    display: {
+      xs: 'block',
+      md: 'flex',
+    },
     '&:before': {
-      content: '""',
+      background: 'rgba(255,255,255,0.75)',
+      borderRadius: '1em',
+      outline: 'solid 0.15em transparent',
+      outlineOffset: '0.15em',
+      lineHeight: '1.75',
       display: 'block',
+      color: 'black',
       width: {
         xs: '3em',
-        md: '1em',
+        md: '1.75em',
       },
       height: {
         xs: '0.75em',
-        md: '1em',
+        md: '1.75em',
       },
-      background: 'white',
-      borderRadius: '1em',
-      outline: 'solid 0.15em transparent',
-      outlineOffset: '0.2em',
     },
     '&.current:before, &.completed:before': {
       background: currentColor,
@@ -60,6 +57,11 @@ const getStepCounterStyles = (currentColor) => ({
     },
   },
   '.title': {
+    margin: {
+      xs: '0',
+      md: '0 0.5em',
+      lg: '0 0.75em',
+    },
     display: {
       xs: 'none',
       md: 'block',
@@ -80,28 +82,53 @@ export const StepCounter = ({ activeStep, currentColor }) => {
           TOTAL_LOG_STEPS
         )}
       </Box>
-      <ol>
+      <Box component="ol">
         {LOG_STEPS.map(({ number, title, name }) => {
-          const { className, statusText } = getStepStatus(activeStep, number);
+          const isCompleted = number < activeStep;
+          const displayNumber = number + 1;
+
+          let className = '',
+            statusText = null;
+
+          if (number === activeStep) {
+            className = 'current';
+            statusText = 'logActionStepCurrent';
+          }
+          if (isCompleted) {
+            className = 'completed';
+            statusText = 'logActionStepCompleted';
+          }
 
           const visuallyHiddenText = [
-            number + 1,
+            displayNumber,
             statusText ? translation[statusText] : '',
             translation[title] ? translation[title] : '',
           ].join(' ');
 
           return (
-            <li key={name} className={className}>
+            <Box
+              component="li"
+              key={name}
+              className={className}
+              sx={{
+                '&:before': {
+                  content: {
+                    xs: '""',
+                    md: `"${isCompleted ? '\\2714' : displayNumber}"`,
+                  },
+                },
+              }}
+            >
               <Typography variant="span" sx={visuallyHidden}>
                 {visuallyHiddenText}
               </Typography>
-              <Typography variant="span" className="title">
+              <Typography variant="span" className="title" aria-hidden="true">
                 {translation[title] ? translation[title] : ''}
               </Typography>
-            </li>
+            </Box>
           );
         })}
-      </ol>
+      </Box>
     </Box>
   );
 };
