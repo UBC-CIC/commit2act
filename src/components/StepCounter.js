a83f3a0eeb5e8dc8dc3fe9d7d3306c2baa1d4993
Stephen Belyea <belyea.stephen@gmail.com>
2023-12-13 10:18:25 -0500

    Shift StepCounter styles out to separate file

--- src/components/StepCounter.js
+++ src/components/StepCounter.js
@@ -3,76 +3,7 @@ import { visuallyHidden } from '@mui/utils';
 import useTranslation from './customHooks/translations';
 import { useActiveStepContext } from '../hooks/use-active-step-context';
 import { LOG_STEPS, TOTAL_LOG_STEPS } from '../constants/log-steps';
-
-const getStepCounterStyles = (color) => ({
-  margin: '0 auto',
-  mt: {
-    xs: '1.5em',
-    sm: '2em',
-    md: '2.5em',
-  },
-  textAlign: 'center',
-  fontSize: {
-    xs: '0.725rem',
-    sm: '0.825rem',
-    lg: '1rem',
-  },
-  ol: {
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    listStyle: 'none',
-    padding: '0',
-    margin: '1em 0',
-    gap: {
-      xs: '0.75em',
-      lg: '1.25em',
-    },
-  },
-  li: {
-    color: 'white',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    display: {
-      xs: 'block',
-      md: 'flex',
-    },
-    '&:before': {
-      background: 'rgba(255,255,255,0.75)',
-      borderRadius: '1em',
-      outline: 'solid 0.15em transparent',
-      outlineOffset: '0.15em',
-      lineHeight: '1.75',
-      display: 'block',
-      color: 'black',
-      width: {
-        xs: '2.25em',
-        md: '1.75em',
-      },
-      height: {
-        xs: '0.5em',
-        md: '1.75em',
-      },
-    },
-    '&.current:before, &.completed:before': {
-      background: color,
-    },
-    '&.current:before': {
-      outlineColor: color,
-    },
-  },
-  '.title': {
-    margin: {
-      xs: '0',
-      md: '0 0.5em',
-      lg: '0 0.75em',
-    },
-    display: {
-      xs: 'none',
-      md: 'block',
-    },
-  },
-});
+import { getStepCounterStyles } from '../styles/step-counter';
 
 export const StepCounter = () => {
   const translation = useTranslation();
