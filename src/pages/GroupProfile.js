6d720d68c5c0f8a0909af3e28cf182d97fc13ccd
Michael Furdyk <mfurdyk@takingitglobal.org>
2023-12-21 16:53:32 -0500

    Ceiling

--- src/pages/GroupProfile.js
+++ src/pages/GroupProfile.js
@@ -289,7 +289,7 @@ const GroupProfile = () => {
                     </Typography>
                     <Typography variant="h5" className="statValue">
                       {groupInfo.total_co2 > 9999
-                        ? `${(groupInfo.total_co2 / 1000).toLocaleString()} kg`
+                        ? `${Math.ceil(groupInfo.total_co2 / 1000).toLocaleString()} kg`
                         : `${Math.ceil(groupInfo.total_co2).toLocaleString()} g`}
                     </Typography>
                   </StyledPaper>
