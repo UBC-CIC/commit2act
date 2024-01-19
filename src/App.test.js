bd50f3dbe3dec34afb3d6707f0ac97586da1767f
Stephen Belyea <belyea.stephen@gmail.com>
2024-01-18 20:21:45 -0500

    Mocking to test initial logged-out state

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MockReduxStoreProvider } from './utils/mock-redux-store-provider';
+import { Box } from '@mui/material';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockSignedOutState = 'signIn';
+const mockUpdateLoginState = jest.fn();
+
+const MockLogin = (props) => <Box {...props} data-testid="login-view" />;
+
+jest.mock('./components/authentication/Login_material', () => (props) => (
+  <MockLogin {...props} />
+));
+
+jest.mock('./components/contexts/ContentTranslationsContext', () => ({
+  useContentTranslationsContext: () => ({
+    setContentTranslations: jest.fn(),
+  }),
+}));
+
+jest.mock('./services/translations', () => ({
+  getAllTranslations: () => ({}),
+}));
+
+describe('App', () => {
+  it('renders Login view when user is not authenticated', () => {
+    render(
+      <MockReduxStoreProvider>
+        <App
+          loginState={mockSignedOutState}
+          updateLoginState={mockUpdateLoginState}
+        />
+      </MockReduxStoreProvider>
+    );
+    expect(screen.getByTestId('login-view')).toBeInTheDocument();
+  });
 });
