2beae56df7e5cd4798fb464b21db491f72af8309
Stephen Belyea <belyea.stephen@gmail.com>
2024-01-23 16:37:30 -0500

    Add basic a11y test for AppNav component with global mock for useLanguageContext hook

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,4 +5,12 @@
 import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
 
+// Extend default expect for jest-axe tests
 expect.extend(toHaveNoViolations);
+
+// Mock common hooks and dependencies
+jest.mock('./components/contexts/LanguageContext', () => ({
+  useLanguageContext: () => ({
+    language: 'en',
+  }),
+}));
